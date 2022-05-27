ad35ed2af07de517849dffd15ced37094bd8d619
Facundo Rojas <facuroja@hotmail.com>
2022-05-27 19:13:01 -0300

    Vemos variables en Java

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -6,5 +6,19 @@ muchas lineas de comentarios
 public class HolaMundo {
     public static void main(String args[]){
         System.out.println("Hola Mundo desde Java");
+        
+        //Defino Variable como Entero
+        int miVariable = 10;
+        System.out.println(miVariable);
+        //Reutilizo la variable
+        miVariable = 5;
+        System.out.println(miVariable);
+        
+        //Variable tipo String
+        String miVariableCadena = "Bienvenidos" ;
+        System.out.println(miVariableCadena);
+        //Reutilizo la variable
+        miVariableCadena = "Seguimos creciendo en programacion";
+        System.out.println(miVariableCadena);
     }
 }
