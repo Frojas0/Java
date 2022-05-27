fe73ffc5d643dbc1642666b6ea57c7ef144e749e
Facundo Rojas <facuroja@hotmail.com>
2022-05-27 12:45:06 -0300

    Creo primer hola mundo de Java

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,10 @@
+//Mi primer programa Hola Mundo
+/*
+Apertura y cierre de comentario para escribir
+muchas lineas de comentarios
+*/
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo desde Java");
+    }
+}
